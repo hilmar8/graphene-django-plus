e2612c19275b547e654366fcd40e30091fe2546a
Hilmar Hilmarsson <hilmarh@gmail.com>
2020-03-03 21:35:10 +0000

    Initial work

--- graphene_django_plus/__init__.py
+++ graphene_django_plus/__init__.py
@@ -1,5 +1,5 @@
-from graphene import Schema, Float, String, Int, Field as RawField  # noqa: F401
+from graphene import ObjectType, Schema, Float, String, Int, Field as RawField  # noqa: F401
 
 __version__ = "0.0.1"
 
-__all__ = ["__version__", "Schema", "Float", "String", "Int", "RawField", ]
+__all__ = ["__version__", "ObjectType", "Schema", "Float", "String", "Int", "RawField", ]
