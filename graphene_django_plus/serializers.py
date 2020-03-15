190c20d2b0e8223fff3485a956b14fe5c6f14668
Hilmar Hilmarsson <hilmarh@gmail.com>
2020-03-15 18:12:48 +0000

    Serializer work

--- graphene_django_plus/serializers.py
+++ graphene_django_plus/serializers.py
@@ -16,7 +16,8 @@ from .registry import get_global_registry
 class SerializerDjangoObjectTypeField(serializers.ReadOnlyField):
     def __init__(self, object_type, **kwargs):
         self.object_type = object_type
-        kwargs["source"] = "*"
+        if "source" not in kwargs:
+            kwargs["source"] = "*"
         super(SerializerDjangoObjectTypeField, self).__init__(**kwargs)
 
 
